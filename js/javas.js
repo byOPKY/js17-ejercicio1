042b544cfbde95d0a96f9f44c11ccfb75b6d95a1
byOPKY <orrillokarol@gmail.com>
2016-08-25 17:27:13 -0500

    subiendo solo el ejercicio!

---
+++ js/javas.js
@@ -0,0 +1,17 @@
+window.alert("Adivina el numero");
+var num1 =parseInt(prompt ("Ingresa el numero"));
+
+if (num1>0) {
+	document.write("Positivo");
+}
+else {
+	document.write("Negativo");
+}
+
+
+
+
+
+
+
+
